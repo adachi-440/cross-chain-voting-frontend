7ef77811a37b06f3f92d0d43e77de86a51e7c83e
adachi_440 <tomoki1999314@gmail.com>
2022-12-11 15:15:52 +0530

    create top page

---
+++ pages/top.tsx
@@ -0,0 +1,40 @@
+import type { NextPage } from 'next'
+import { Container, Card, Row, Text, Col, Spacer, Button } from '@nextui-org/react'
+import styles from '../styles/Top.module.css'
+import Image from 'next/image'
+import voting from '../public/voting.svg'
+
+// TODO this file is index.tsx
+const Top: NextPage = () => {
+  return (
+    <Container fluid>
+      <Row gap={6}>
+        <Col>
+          <Text weight={'bold'} size={40}>
+            Gasless & Omnichain voting system
+          </Text>
+          <Spacer y={3} />
+          <Text weight={'medium'} size={24}>
+            Cross-chain messaging allows for on-chain voting from any chain.Users can participate in voting without
+            worrying about gas costs.
+          </Text>
+          <Spacer y={2} />
+          <Button
+            auto
+            rounded
+            css={{
+              background: '#0841D4',
+            }}
+          >
+            Try Out
+          </Button>
+        </Col>
+        <Col>
+          <Image src={voting} alt='Picture of the author' width={400} height={400} />
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Top
